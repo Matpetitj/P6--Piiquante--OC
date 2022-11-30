f4b38b4db75019cc001dae6eecf6d6f053249f69
Mathis Petitjean <veille.petitjean@gmail.com>
2022-11-30 23:50:05 +0100

    Checkpoint

--- backend/rooter/sauce-rooter.js
+++ backend/rooter/sauce-rooter.js
@@ -6,8 +6,8 @@ const checkSauceInput = require("../middleware/sauce-input-middleware");
 
 const sauceCtrl = require("../controllers/sauce-controller");
 
-router.get("/", auth, sauceCtrl.getAllSauces);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get("/", sauceCtrl.getAllSauces);
+router.get('/:id', sauceCtrl.getOneSauce);
 router.post("/", auth, multer, checkSauceInput, sauceCtrl.createSauce);
 router.put("/:id", auth, multer, checkSauceInput, sauceCtrl.updateSauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
