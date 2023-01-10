700078e727969af732c70a10ce5d5842fff22e78
Mathis Petitjean <veille.petitjean@gmail.com>
2023-01-10 23:19:34 +0100

    Checkpoint

--- backend/middleware/sauce-input-middleware.js
+++ backend/middleware/sauce-input-middleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    // Si il s'agit de la route post
+    // il s'agit de la route post
     if (JSON.parse(req.body.sauce !== undefined)) {
       const sauce = JSON.parse(req.body.sauce);
       let { name, manufacturer, description, mainPepper } = sauce;
@@ -10,15 +10,15 @@ module.exports = (req, res, next) => {
       }
       toTrim(name, manufacturer, description, mainPepper);
   
-      // Vérification du nombre de caractères après avoir trim()
-      const hasThreeCharacters = (currentValue) => currentValue.length >= 3;
-      if (trimedTab.every(hasThreeCharacters)) {
+      // Vérifie du nombre de caractères après avoir trim()
+      const minThreeCharacters = (currentValue) => currentValue.length >= 3;
+      if (trimedTab.every(minThreeCharacters)) {
         next();
       } else {
         throw new Error("Tous les champs doivent faire au moins 3 caractères");
       }
     } else {
-      // Si il s'agit de la route put
+      // il s'agit de la route put
       const sauce = req.body;
       let { name, manufacturer, description, mainPepper } = sauce;
       let trimedTab = [];
@@ -28,9 +28,9 @@ module.exports = (req, res, next) => {
       }
       toTrim(name, manufacturer, description, mainPepper);
   
-      // Vérification du nombre de caractères après avoir trim()
-      const hasThreeCharacters = (currentValue) => currentValue.length >= 3;
-      if (trimedTab.every(hasThreeCharacters)) {
+      // Vérifie du nombre de caractères après avoir trim()
+      const minThreeCharacters = (currentValue) => currentValue.length >= 3;
+      if (trimedTab.every(minThreeCharacters)) {
         next();
       } else {
         throw new Error("Tous les champs doivent faire au moins 3 caractères");
--- backend/rooter/sauce-rooter.js
+++ backend/rooter/sauce-rooter.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const multer = require("../middleware/multer-config-middleware");
 const auth = require("../middleware/auth-middleware");
-const checkSauceInput = require("../middleware/sauce-input-middleware");
+const sauceInput = require("../middleware/sauce-input-middleware");
 
 const sauceCtrl = require("../controllers/sauce-controller");
 
 router.get("/", sauceCtrl.getAllSauces);
 router.get('/:id', sauceCtrl.getOneSauce);
-router.post("/", auth, multer, checkSauceInput, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, checkSauceInput, sauceCtrl.updateSauce)
+router.post("/", auth, multer, sauceInput, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceInput, sauceCtrl.updateSauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
 router.post("/:id/like", auth, sauceCtrl.likeAndDislikeSauce)
 
